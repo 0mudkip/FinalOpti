ddff5317f0e584b6f1c30cecbff9e99847ead822
0mudkip <180235-a@iberoleon.edu.mx>
2025-04-27 19:46:11 -0600

    Se añadio función objetivo
    Se programo la función objetivo y se cambio el ruido para que se viera mejor, aparte se añadio la imagen

--- ruidoimagen.py
+++ ruidoimagen.py
@@ -1,12 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import img_as_float, img_as_ubyte
+from skimage import img_as_float
 from skimage.io import imread
+from scipy.ndimage import sobel
 
+
+def funcion_objetivo(u, f, lambda_reg):
+    # Primer término: error de datos (diferencia entre u y f)
+    data_term = 1/2 * np.sum((u - f)**2)
+
+    # Gradiente de u
+    grad_x = sobel(u, axis=0, mode='reflect')  # Derivada en x
+    grad_y = sobel(u, axis=1, mode='reflect')  # Derivada en y
+    grad_term = 0.5 * lambda_reg * np.sum(grad_x**2 + grad_y**2)
+
+    # Función objetivo
+    return data_term + grad_term
 
 imagen = imread('prueba1.jpg', as_gray=True) 
 imagen = img_as_float(imagen) 
-nivel_ruido = 0.1  
+nivel_ruido = 0.5  
 
 # Crear ruido gaussiano
 ruido = np.random.normal(0, nivel_ruido, imagen.shape)
