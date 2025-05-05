9b8c629807326f4130c69f906f3bd4c53c2944b5
0mudkip <180235-a@iberoleon.edu.mx>
2025-05-04 23:52:39 -0600

    Se implemento el algoritmo de gradiente
    Se implementaron los 3 algoritmos de gradiente, se hicieron pruebas y se agregaron mas imagenes pero todavia no jala al 100 :(

---
+++ principal.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage import img_as_float
+from gradiente import descenso_gradiente_simple, descenso_gradiente_momentum, descenso_gradiente_nesterov
+
+#imagen = imread('prueba1.jpeg', as_gray=True)
+#imagen = imread('prueba2.jpg', as_gray=True)
+imagen = imread('prueba3.jpg', as_gray=True)
+imagen = img_as_float(imagen)
+nivel_ruido = 0.25
+ruido = np.random.normal(0, nivel_ruido, imagen.shape)
+imagen_ruidosa = imagen + ruido
+imagen_ruidosa = np.clip(imagen_ruidosa, 0, 1)
+
+
+lambda_param = 0.5  
+alpha = 0.1 
+beta = 0.9  # Momentum
+num_iter = 10000  # Número de iteraciones
+
+u_inicial = np.copy(imagen_ruidosa)
+
+# 1. Descenso de gradiente simple
+u_simple = descenso_gradiente_simple(u_inicial, imagen_ruidosa, lambda_param, alpha, num_iter)
+
+# 2. Descenso de gradiente con momentum
+u_momentum = descenso_gradiente_momentum(u_inicial, imagen_ruidosa, lambda_param, alpha, beta, num_iter)
+
+# 3. Descenso de gradiente con Nesterov
+u_nesterov = descenso_gradiente_nesterov(u_inicial, imagen_ruidosa, lambda_param, alpha, beta, num_iter)
+
+# Mostrar resultados
+plt.figure(figsize=(15,10))
+
+# Imagen original
+plt.subplot(2, 3, 1)
+plt.title('Imagen Original')
+plt.imshow(imagen, cmap='gray')
+plt.axis('off')
+
+# Imagen con ruido
+plt.subplot(2, 3, 2)
+plt.title('Imagen Ruidosa')
+plt.imshow(imagen_ruidosa, cmap='gray')
+plt.axis('off')
+
+# Resultado Descenso Gradiente Simple
+plt.subplot(2, 3, 3)
+plt.title('Denoising: Gradiente Simple')
+plt.imshow(u_simple, cmap='gray')
+plt.axis('off')
+
+# Resultado Descenso Gradiente con Momentum
+plt.subplot(2, 3, 4)
+plt.title('Denoising: Momentum')
+plt.imshow(u_momentum, cmap='gray')
+plt.axis('off')
+
+# Resultado Descenso Gradiente con Nesterov
+plt.subplot(2, 3, 5)
+plt.title('Denoising: Nesterov')
+plt.imshow(u_nesterov, cmap='gray')
+plt.axis('off')
+
+plt.show()
--- ruidoimagen.py
+++ ruidoimagen.py
@@ -5,19 +5,7 @@ from skimage.io import imread
 from scipy.ndimage import sobel
 
 
-def funcion_objetivo(u, f, lambda_reg):
-    # Primer término: error de datos (diferencia entre u y f)
-    data_term = 1/2 * np.sum((u - f)**2)
-
-    # Gradiente de u
-    grad_x = sobel(u, axis=0, mode='reflect')  # Derivada en x
-    grad_y = sobel(u, axis=1, mode='reflect')  # Derivada en y
-    grad_term = 0.5 * lambda_reg * np.sum(grad_x**2 + grad_y**2)
-
-    # Función objetivo
-    return data_term + grad_term
-
-imagen = imread('prueba1.jpg', as_gray=True) 
+imagen = imread('prueba2.jpg', as_gray=True) 
 imagen = img_as_float(imagen) 
 nivel_ruido = 0.5  
 
